a7946d511d76f53b6de5d6c708bcedce26f3c97a
DUPIRE Matthieu <dupire.matthieu@gmail.com>
2025-07-17 23:12:37 +0200

    Update Ventes par Régions.sql

--- Ventes par Régions.sql
+++ Ventes par Régions.sql
@@ -1,4 +1,7 @@
+# query: je demande à la base de données "ventes" de me retourner 3 champs: la région (c5), la somme du nombre de produits vendus par région (SUM(c4)) ainsi que le chiffre d'affaire par région (SUM(c3*c4))
+# je lui demande de les trier selon la région (GROUP BY c5), et de supprimer l'enregistrement "region" du résultat pour une meilleure lisibilité (WHERE c5 != 'region')  
+
 SELECT c5 , SUM(c4) AS ventes_par_region , SUM(c3*c4) AS chiffre_d_affaire_par_region
 FROM ventes
-where c5 != 'region'
-group by c5
+WHERE c5 != 'region'
+GROUP BY c5
