13e796e7530ca1adca90cace1e6531f9aa30da34
DUPIRE Matthieu <dupire.matthieu@gmail.com>
2025-07-17 23:33:23 +0200

    Update chiffre d'affaires par produit.py

--- chiffre d'affaires par produit.py
+++ chiffre d'affaires par produit.py
@@ -1,3 +1,9 @@
+# Pour ce programme j'ai repris le précédent programme proposé dans l'exercice en y apportant des modifications: dans un premier temps je crée une nouvelle "données":
+# je crée la donnée 'chiffre_affaires' comme étant le produit de la donnée déjà existante 'qte' et de la donnée existante 'prix'. Ensuite dans la variable figure
+# j'utilise 'chiffre_affaires' en 'values', 'produits' en 'names' et je modifie le titre par 'chiffre affaires par produit'. De la même manière je modifie le nom du 
+# fichier html créé par 'chiffre-affaires-par-produit.html'.
+# Le résultat sous forme de graphique est disponible dans le document Word "Projet_Analyse_des_ventes_PME_DUPIRE_Matthieu.docx" disponible dans le Github.
+
 import plotly.express as px
 import pandas as pd
 
