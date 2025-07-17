86991bbbb008d4ae42500314ed2f66bc97f062bc
DUPIRE Matthieu <dupire.matthieu@gmail.com>
2025-07-17 23:13:04 +0200

    Update chiffre d'affaire total.sql

--- chiffre d'affaire total.sql
+++ chiffre d'affaire total.sql
@@ -1,4 +1,4 @@
 # query: je demande la somme des prix de ventes de tous les produits (c3*c4) que je nomme "chiffre_d_affaire_total" depuis la table "ventes"
 
-select SUM(c3*c4) AS chiffre_d_affaire_total
-from ventes
+SELECT SUM(c3*c4) AS chiffre_d_affaire_total
+FROM ventes
